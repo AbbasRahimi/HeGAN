dd08f75ebed4a85230599c80c462491e99040d2a
AbbasRahimi <Abbasrahimi71@gmail.com>
2021-06-26 12:45:04 +0430

    rename code folder to source_code

--- source_code/he_gan.py
+++ source_code/he_gan.py
@@ -20,8 +20,9 @@ class Model():
         t = time.time()
         print("reading graph...")
         self.n_node, self.n_relation, self.graph = utils.read_graph(config.graph_filename)
-        self.node_list = self.graph.keys()#range(0, self.n_node)
-        print('[%.2f] reading graph finished. #node = %d #relation = %d' % (time.time() - t, self.n_node, self.n_relation))
+        self.node_list = self.graph.keys()  # range(0, self.n_node)
+        print('[%.2f] reading graph finished. #node = %d #relation = %d' % (
+        time.time() - t, self.n_node, self.n_relation))
 
         t = time.time()
         print("read initial embeddings...")
@@ -32,10 +33,10 @@ class Model():
                                                        n_node=self.n_node,
                                                        n_embed=config.n_emb)
 
-        #self.rel_embed_init_d = utils.read_embeddings(filename=config.pretrain_rel_emb_filename_d,
+        # self.rel_embed_init_d = utils.read_embeddings(filename=config.pretrain_rel_emb_filename_d,
         #                                              n_node=self.n_node,
         #                                              n_embed=config.n_emb)
-        #self.rel_embed_init_g = utils.read_embeddings(filename=config.pretrain_rel_emb_filename_g,
+        # self.rel_embed_init_g = utils.read_embeddings(filename=config.pretrain_rel_emb_filename_g,
         #                                              n_node=self.n_node,
         #                                              n_embed=config.n_emb)
         print("[%.2f] read initial embeddings finished." % (time.time() - t))
@@ -56,7 +57,7 @@ class Model():
         self.config = tf.ConfigProto()
         self.config.gpu_options.allow_growth = True
         self.init_op = tf.group(tf.global_variables_initializer(), tf.local_variables_initializer())
-        self.sess = tf.Session(config = self.config)
+        self.sess = tf.Session(config=self.config)
         self.sess.run(self.init_op)
 
         self.show_config()
@@ -78,18 +79,20 @@ class Model():
         print('sig = ', config.sig)
         print('label smooth = ', config.label_smooth)
         print('--------------------')
+
     def build_generator(self):
-        #with tf.variable_scope("generator"):
-        self.generator = generator.Generator(n_node = self.n_node,
-                                             n_relation = self.n_relation,
-                                             node_emd_init = self.node_embed_init_g,
-                                             relation_emd_init = None)
+        # with tf.variable_scope("generator"):
+        self.generator = generator.Generator(n_node=self.n_node,
+                                             n_relation=self.n_relation,
+                                             node_emd_init=self.node_embed_init_g,
+                                             relation_emd_init=None)
+
     def build_discriminator(self):
-        #with tf.variable_scope("discriminator"):
-        self.discriminator = discriminator.Discriminator(n_node = self.n_node,
-                                                         n_relation = self.n_relation,
-                                                         node_emd_init = self.node_embed_init_d,
-                                                         relation_emd_init = None)
+        # with tf.variable_scope("discriminator"):
+        self.discriminator = discriminator.Discriminator(n_node=self.n_node,
+                                                         n_relation=self.n_relation,
+                                                         node_emd_init=self.node_embed_init_d,
+                                                         relation_emd_init=None)
 
     def train(self):
 
@@ -102,8 +105,8 @@ class Model():
             one_epoch_dis_loss = 0.0
             one_epoch_batch_num = 0.0
 
-            #D-step
-            #t1 = time.time()
+            # D-step
+            # t1 = time.time()
             for d_epoch in range(config.d_epoch):
                 np.random.shuffle(self.node_list)
                 one_epoch_dis_loss = 0.0
@@ -112,98 +115,104 @@ class Model():
                 one_epoch_neg_loss_2 = 0.0
 
                 for index in range(len(self.node_list) / config.batch_size):
-                    #t1 = time.time()
-                    pos_node_ids, pos_relation_ids, pos_node_neighbor_ids, neg_node_ids_1, neg_relation_ids_1, neg_node_neighbor_ids_1, neg_node_ids_2, neg_relation_ids_2, node_fake_neighbor_embedding = self.prepare_data_for_d(index)
-                    #t2 = time.time()
-                    #print(t2 - t1
-                    _, dis_loss, pos_loss, neg_loss_1, neg_loss_2 = self.sess.run([self.discriminator.d_updates, self.discriminator.loss, self.discriminator.pos_loss, self.discriminator.neg_loss_1, self.discriminator.neg_loss_2],
-                                                 feed_dict = {self.discriminator.pos_node_id : np.array(pos_node_ids),
-                                                              self.discriminator.pos_relation_id : np.array(pos_relation_ids),
-                                                              self.discriminator.pos_node_neighbor_id : np.array(pos_node_neighbor_ids),
-                                                              self.discriminator.neg_node_id_1 : np.array(neg_node_ids_1),
-                                                              self.discriminator.neg_relation_id_1 : np.array(neg_relation_ids_1),
-                                                              self.discriminator.neg_node_neighbor_id_1 : np.array(neg_node_neighbor_ids_1),
-                                                              self.discriminator.neg_node_id_2 : np.array(neg_node_ids_2),
-                                                              self.discriminator.neg_relation_id_2 : np.array(neg_relation_ids_2),
-                                                              self.discriminator.node_fake_neighbor_embedding : np.array(node_fake_neighbor_embedding)})
+                    # t1 = time.time()
+                    pos_node_ids, pos_relation_ids, pos_node_neighbor_ids, neg_node_ids_1, neg_relation_ids_1, neg_node_neighbor_ids_1, neg_node_ids_2, neg_relation_ids_2, node_fake_neighbor_embedding = self.prepare_data_for_d(
+                        index)
+                    # t2 = time.time()
+                    # print(t2 - t1
+                    _, dis_loss, pos_loss, neg_loss_1, neg_loss_2 = self.sess.run(
+                        [self.discriminator.d_updates, self.discriminator.loss, self.discriminator.pos_loss,
+                         self.discriminator.neg_loss_1, self.discriminator.neg_loss_2],
+                        feed_dict={self.discriminator.pos_node_id: np.array(pos_node_ids),
+                                   self.discriminator.pos_relation_id: np.array(pos_relation_ids),
+                                   self.discriminator.pos_node_neighbor_id: np.array(pos_node_neighbor_ids),
+                                   self.discriminator.neg_node_id_1: np.array(neg_node_ids_1),
+                                   self.discriminator.neg_relation_id_1: np.array(neg_relation_ids_1),
+                                   self.discriminator.neg_node_neighbor_id_1: np.array(neg_node_neighbor_ids_1),
+                                   self.discriminator.neg_node_id_2: np.array(neg_node_ids_2),
+                                   self.discriminator.neg_relation_id_2: np.array(neg_relation_ids_2),
+                                   self.discriminator.node_fake_neighbor_embedding: np.array(
+                                       node_fake_neighbor_embedding)})
 
                     one_epoch_dis_loss += dis_loss
                     one_epoch_pos_loss += pos_loss
                     one_epoch_neg_loss_1 += neg_loss_1
                     one_epoch_neg_loss_2 += neg_loss_2
 
-            #G-step
+            # G-step
 
             for g_epoch in range(config.g_epoch):
                 np.random.shuffle(self.node_list)
                 one_epoch_gen_loss = 0.0
 
                 for index in range(len(self.node_list) / config.batch_size):
-
-                    gen_node_ids, gen_relation_ids, gen_noise_embedding, gen_dis_node_embedding, gen_dis_relation_embedding = self.prepare_data_for_g(index)
+                    gen_node_ids, gen_relation_ids, gen_noise_embedding, gen_dis_node_embedding, gen_dis_relation_embedding = self.prepare_data_for_g(
+                        index)
                     t2 = time.time()
 
                     _, gen_loss = self.sess.run([self.generator.g_updates, self.generator.loss],
-                                                 feed_dict = {self.generator.node_id :  np.array(gen_node_ids),
-                                                              self.generator.relation_id :  np.array(gen_relation_ids),
-                                                              self.generator.noise_embedding : np.array(gen_noise_embedding),
-                                                              self.generator.dis_node_embedding : np.array(gen_dis_node_embedding),
-                                                              self.generator.dis_relation_embedding : np.array(gen_dis_relation_embedding)})
+                                                feed_dict={self.generator.node_id: np.array(gen_node_ids),
+                                                           self.generator.relation_id: np.array(gen_relation_ids),
+                                                           self.generator.noise_embedding: np.array(
+                                                               gen_noise_embedding),
+                                                           self.generator.dis_node_embedding: np.array(
+                                                               gen_dis_node_embedding),
+                                                           self.generator.dis_relation_embedding: np.array(
+                                                               gen_dis_relation_embedding)})
 
                     one_epoch_gen_loss += gen_loss
 
             one_epoch_batch_num = len(self.node_list) / config.batch_size
 
-            #print(t2 - t1
-            #exit()
+            # print(t2 - t1
+            # exit()
             print('[%.2f] gen loss = %.4f, dis loss = %.4f pos loss = %.4f neg loss-1 = %.4f neg loss-2 = %.4f' % \
-                    (time.time() - t, one_epoch_gen_loss / one_epoch_batch_num, one_epoch_dis_loss / one_epoch_batch_num,
-                    one_epoch_pos_loss / one_epoch_batch_num, one_epoch_neg_loss_1 / one_epoch_batch_num, one_epoch_neg_loss_2 / one_epoch_batch_num))
-
+                  (time.time() - t, one_epoch_gen_loss / one_epoch_batch_num, one_epoch_dis_loss / one_epoch_batch_num,
+                   one_epoch_pos_loss / one_epoch_batch_num, one_epoch_neg_loss_1 / one_epoch_batch_num,
+                   one_epoch_neg_loss_2 / one_epoch_batch_num))
 
             if config.dataset == 'dblp':
                 gen_nmi, dis_nmi = self.evaluate_author_cluster()
                 print('Gen NMI score = %.4f Dis NMI score = %.4f' % (gen_nmi, dis_nmi))
-                #micro_f1s, macro_f1s = self.evaluate_author_classification()
-                #print('Gen Micro_f1 = %.4f Dis Micro_f1 = %.4f' %(micro_f1s[0], micro_f1s[1])
-                #print('Gen Macro_f1 = %.4f Dis Macro_f1 = %.4f' %(macro_f1s[0], macro_f1s[1])
+                # micro_f1s, macro_f1s = self.evaluate_author_classification()
+                # print('Gen Micro_f1 = %.4f Dis Micro_f1 = %.4f' %(micro_f1s[0], micro_f1s[1])
+                # print('Gen Macro_f1 = %.4f Dis Macro_f1 = %.4f' %(macro_f1s[0], macro_f1s[1])
             elif config.dataset == 'yelp':
                 gen_nmi, dis_nmi = self.evaluate_business_cluster()
                 print('Gen NMI score = %.4f Dis NMI score = %.4f' % (gen_nmi, dis_nmi))
-                #micro_f1s, macro_f1s = self.evaluate_business_classification()
-                #print('Gen Micro_f1 = %.4f Dis Micro_f1 = %.4f' %(micro_f1s[0], micro_f1s[1])
-                #print('Gen Macro_f1 = %.4f Dis Macro_f1 = %.4f' %(macro_f1s[0], macro_f1s[1])
+                # micro_f1s, macro_f1s = self.evaluate_business_classification()
+                # print('Gen Micro_f1 = %.4f Dis Micro_f1 = %.4f' %(micro_f1s[0], micro_f1s[1])
+                # print('Gen Macro_f1 = %.4f Dis Macro_f1 = %.4f' %(macro_f1s[0], macro_f1s[1])
             elif config.dataset == 'aminer':
                 gen_nmi, dis_nmi = self.evaluate_paper_cluster()
                 print('Gen NMI score = %.4f Dis NMI score = %.4f' % (gen_nmi, dis_nmi))
-                #micro_f1s, macro_f1s = self.evaluate_paper_classification()
-                #print('Gen Micro_f1 = %.4f Dis Micro_f1 = %.4f' %(micro_f1s[0], micro_f1s[1])
-                #print('Gen Macro_f1 = %.4f Dis Macro_f1 = %.4f' %(macro_f1s[0], macro_f1s[1])
+                # micro_f1s, macro_f1s = self.evaluate_paper_classification()
+                # print('Gen Micro_f1 = %.4f Dis Micro_f1 = %.4f' %(micro_f1s[0], micro_f1s[1])
+                # print('Gen Macro_f1 = %.4f Dis Macro_f1 = %.4f' %(macro_f1s[0], macro_f1s[1])
 
-            #self.evaluate_aminer_link_prediction()
-            #self.write_embeddings_to_file(epoch)
-            #os.system('python ../evaluation/lp_evaluation_2.py')
-
+            # self.evaluate_aminer_link_prediction()
+            # self.write_embeddings_to_file(epoch)
+            # os.system('python ../evaluation/lp_evaluation_2.py')
 
         print("training completes")
+
     def prepare_data_for_d(self, index):
 
         pos_node_ids = []
         pos_relation_ids = []
         pos_node_neighbor_ids = []
 
-        #real node and wrong relation
+        # real node and wrong relation
         neg_node_ids_1 = []
         neg_relation_ids_1 = []
         neg_node_neighbor_ids_1 = []
 
-        #fake node and true relation
+        # fake node and true relation
         neg_node_ids_2 = []
         neg_relation_ids_2 = []
         node_fake_neighbor_embedding = None
 
-
-        for node_id in self.node_list[index * config.batch_size : (index + 1) * config.batch_size]:
+        for node_id in self.node_list[index * config.batch_size: (index + 1) * config.batch_size]:
             for i in range(config.n_sample):
 
                 # sample real node and true relation
@@ -216,7 +225,7 @@ class Model():
                 pos_relation_ids.append(relation_id)
                 pos_node_neighbor_ids.append(node_neighbor_id)
 
-                #sample real node and wrong relation
+                # sample real node and wrong relation
                 neg_node_ids_1.append(node_id)
                 neg_node_neighbor_ids_1.append(node_neighbor_id)
                 neg_relation_id_1 = np.random.randint(0, self.n_relation)
@@ -224,7 +233,7 @@ class Model():
                     neg_relation_id_1 = np.random.randint(0, self.n_relation)
                 neg_relation_ids_1.append(neg_relation_id_1)
 
-                #sample fake node and true relation
+                # sample fake node and true relation
                 neg_node_ids_2.append(node_id)
                 neg_relation_ids_2.append(relation_id)
 
@@ -232,9 +241,11 @@ class Model():
         noise_embedding = np.random.normal(0.0, config.sig, (len(neg_node_ids_2), config.n_emb))
 
         node_fake_neighbor_embedding = self.sess.run(self.generator.node_neighbor_embedding,
-                                                     feed_dict = {self.generator.node_id : np.array(neg_node_ids_2),
-                                                                  self.generator.relation_id : np.array(neg_relation_ids_2),
-                                                                  self.generator.noise_embedding : np.array(noise_embedding)})
+                                                     feed_dict={self.generator.node_id: np.array(neg_node_ids_2),
+                                                                self.generator.relation_id: np.array(
+                                                                    neg_relation_ids_2),
+                                                                self.generator.noise_embedding: np.array(
+                                                                    noise_embedding)})
 
         return pos_node_ids, pos_relation_ids, pos_node_neighbor_ids, neg_node_ids_1, neg_relation_ids_1, \
                neg_node_neighbor_ids_1, neg_node_ids_2, neg_relation_ids_2, node_fake_neighbor_embedding
@@ -253,11 +264,11 @@ class Model():
 
         noise_embedding = np.random.normal(0.0, config.sig, (len(node_ids), config.n_emb))
 
-        dis_node_embedding, dis_relation_embedding = self.sess.run([self.discriminator.pos_node_embedding, self.discriminator.pos_relation_embedding],
-                                                                    feed_dict = {self.discriminator.pos_node_id : np.array(node_ids),
-                                                                                 self.discriminator.pos_relation_id : np.array(relation_ids)})
+        dis_node_embedding, dis_relation_embedding = self.sess.run(
+            [self.discriminator.pos_node_embedding, self.discriminator.pos_relation_embedding],
+            feed_dict={self.discriminator.pos_node_id: np.array(node_ids),
+                       self.discriminator.pos_relation_id: np.array(relation_ids)})
         return node_ids, relation_ids, noise_embedding, dis_node_embedding, dis_relation_embedding
-
 
     def evaluate_author_cluster(self):
         modes = [self.generator, self.discriminator]
@@ -328,8 +339,8 @@ class Model():
         for i in range(2):
             embedding_matrix = self.sess.run(modes[i].node_embedding_matrix)
 
-            #score = self.yelp_evaluation.evaluate_business_cluster(embedding_matrix)
-            #print('%d nmi = %.4f' % (i, score)
+            # score = self.yelp_evaluation.evaluate_business_cluster(embedding_matrix)
+            # print('%d nmi = %.4f' % (i, score)
 
             auc, f1, acc = self.yelp_evaluation.evaluation_link_prediction(embedding_matrix)
 
@@ -340,12 +351,11 @@ class Model():
 
         for i in range(2):
             embedding_matrix = self.sess.run(modes[i].node_embedding_matrix)
-            #relation_matrix = self.sess.run(modes[i].relation_embedding_matrix)
+            # relation_matrix = self.sess.run(modes[i].relation_embedding_matrix)
 
             auc, f1, acc = self.dblp_evaluation.evaluation_link_prediction(embedding_matrix)
 
             print('auc = %.4f f1 = %.4f acc = %.4f' % (auc, f1, acc))
-
 
     def write_embeddings_to_file(self, epoch):
         modes = [self.generator, self.discriminator]
@@ -354,12 +364,12 @@ class Model():
             index = np.array(range(self.n_node)).reshape(-1, 1)
             embedding_matrix = np.hstack([index, embedding_matrix])
             embedding_list = embedding_matrix.tolist()
-            embedding_str = [str(int(emb[0])) + ' ' + ' '.join([str(x) for x in emb[1:]]) + '\n' for emb in embedding_list]
+            embedding_str = [str(int(emb[0])) + ' ' + ' '.join([str(x) for x in emb[1:]]) + '\n' for emb in
+                             embedding_list]
 
             with open(config.emb_filenames[i], 'w') as f:
                 lines = [str(self.n_node) + ' ' + str(config.n_emb) + '\n'] + embedding_str
                 f.writelines(lines)
-
 
 
 if __name__ == '__main__':
